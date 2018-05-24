2e7767c1a588d6745789f35769da27b023697bd6
Carmelo Piccione <carmelo.piccione@struktured.io>
2018-05-24 16:59:52 -0400

    more bindings

--- src/native.ml
+++ src/native.ml
@@ -23,7 +23,7 @@ external verify_signature :
   "verifySignature"
 
 
-(** Attempts to commit an entry to your local source chain. It will cause callback to your validateCommitfunction. Returns either an error or the hash of the committed entry upon success. The type of the entryData parameter depends on the entry format of entry. If it's a string entry format then the type must be string. If it's a JSON entry format, then it can by any type, and the value will get appropriately converted to JSON. If it is a links format entry, then the type must by a JSON object.
+(** Attempts to commit an entry to your local source chain. It will cause callbac to your validaneCommitfunction. Returns either an error or the hash of the committed entry upon success. The type of the entryData parameter depends on the entry format of entry. If it's a string entry format then the type must be string. If it's a JSON entry format, then it can by any type, and the value will get appropriately converted to JSON. If it is a links format entry, then the type must by a JSON object.
 
 A linksentry object looks like this
 
@@ -61,3 +61,44 @@ class type bridge =
 (**This function allows your app to examine which bridges have been put in place. *)
 external get_bridges :
   unit -> bridge array = "getBridges" [@@bs.val]
+
+(**
+ *This function retrieves an entry from the local chain or the DHT. If options.StatusMask is present, it determines which entries to return, depending on their status. If options.GetMask is present, this option allows you to specify what information about the entry you want. For more on that, see Entry Objects and Masks.
+
+If options.Local is set to true, it indicates that the get refers to the local chain only. This allows you to retrieve specific entries from your chain, which includes private entries that aren't published to the DHT.
+
+If options.Bundle is set to true, it indicates that the get refers to the currently started bundle only. If no bundle has been started, returns an error.
+
+If only StatusMask value specified or only Entry is specified the return value will be the actual entry value. Otherwise the return value will be an object with properties of the same name as the mask name.
+
+hash: hash-string
+options: object (optional)
+options.StatusMask: Status-int
+options.GetMask: Mask-int
+options.Local: boolean
+options.Bundle: boolean
+Returns: Entry-object OR HC.HashNotFound
+*)
+external get :
+  hash_string -> options:'a Js.t -> 'entry Js.t =
+  "get" [@@bs.val]
+
+
+(**Retrieves a list of links tagged as tag on base from the DHT. If tag is an empty string it will return all the links on the baseand the list will also include the Tag property on entries. With options as {Load: false} (which is the default) returns a list of the form [{Hash:"QmY..."},..] With options as {Load: true} it will get the entry values of the links and return a list of the form [{Hash:"QmY...",EntryType:"<entry-type>",Entry:"<entry value here>",Source:"<source-hash>"},..]}. Use options.StatusMask to return only links with a certain status. Default is to return only Live links. You can use defined constants HC.Status.Live/Deleted/Rejected as the int value. *)
+
+external get_links  :
+  base:hash_string -> tag:string -> options:'a Js.t -> 'entry Js.t array =
+  "getLinks"
+
+(** Commits a DelEntry to the local chain with given delete message, and, if the entry type of entry is not private, moves the entry to the Deleted status on the DHT. *)
+external remove : entry:'obj Js.t -> message:string -> hash_string =
+  "remove" [@@bs.val]
+
+(** Attempts to commit an entry to your local source chain that "replaces" a previous entry. If entryType is not private, update will movereplaces to a Modifiedstatus on the DHT. Additionally the modification action will be recorded in the entries' header in the local chain, which will be used by validation routes. **)
+external update : entry_type:string -> entry_data:'string_or_obj Js.t ->
+  'string_or_obj Js.t = "update"
+
+external update_agent :
+  options: 'obj Js.t -> hash_string (* or-error *) = "updateAgent" [@@bs.val]
+
+

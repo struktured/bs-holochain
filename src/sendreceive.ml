257a3fd191959ff19d73da8ef1169383ec47206f
Carmelo Piccione <carmelo.piccione@struktured.io>
2018-06-12 23:07:35 -0400

    add missing hashString argument to send function

--- src/sendreceive.ml
+++ src/sendreceive.ml
@@ -9,19 +9,27 @@ module type S0 =
 sig
   type input (** the arguments to [send] *)
   type output (** the output returned by [receive] *)
+
+  (** Callback to receive a message via the send / receive protocol.
+      The [hashString] is the sender address. The callback must
+      implement a value from type [output] derived from type [input].
+
+      It may use other native holochain functions to accomplish, including
+      mutating operations.
+  *)
   val receive : hashString -> input -> output
 end
 
 module type S = sig
   include S0
-  val send : input -> output
+  val send : hashString -> input -> output
 end
 
 module Make (T : S0) :
   S with type input = T.input with type output = T.output =
 struct
   include T
-  external send : input -> output = "" [@@bs.val]
+  external send : hashString -> input -> output = "" [@@bs.val]
   let send = send
 end
 
